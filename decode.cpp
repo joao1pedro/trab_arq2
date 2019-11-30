48a9da15438e9da9eb487d1d144b3fc64fba36e7
João Pedro <jpedro@alu.ufc.br>
2019-11-30 19:39:41 -0300

    update

--- decode.cpp
+++ decode.cpp
@@ -13,128 +13,179 @@ using namespace std;
 // for string delimiter
 vector<string> split(string s, string delimiter)
 {
-	size_t pos_start = 0, pos_end, delim_len = delimiter.length();
-	string token;
-	vector<string> res;
+    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+    string token;
+    vector<string> res;
 
-	while ((pos_end = s.find(delimiter, pos_start)) != string::npos)
-	{
-		token = s.substr(pos_start, pos_end - pos_start);
-		pos_start = pos_end + delim_len;
-		res.push_back(token);
-	}
+    while ((pos_end = s.find(delimiter, pos_start)) != string::npos)
+    {
+        token = s.substr(pos_start, pos_end - pos_start);
+        pos_start = pos_end + delim_len;
+        res.push_back(token);
+    }
 
-	res.push_back(s.substr(pos_start));
-	return res;
+    res.push_back(s.substr(pos_start));
+    return res;
 }
 
 int main()
 {
-	string line;
-	ifstream in("in.s");
-	ofstream out;
+    string line;
+    ifstream in("in.s");
+    ofstream out;
 
-	short valor = 0;
+    short valor = 0;
 
-	if (in.is_open())
-	{
-		while (getline(in, line))
-		{
-			transform(line.begin(), line.end(), line.begin(), ::toupper); //tranforma tudo em maiúscula
-			vector<string> inst = split(line, " ");
-			out.open("out.txt");
-			if (inst[0] == "MOV")
-			{
-				if (inst[1][0] == 'R')
-				{
-					if (inst[2][0] == '#')
-					{   //identificou a instrução MOV da 5º linha da tabela
-						//colocando o valor imediato no short(16 bits) nos 7 primeiros bitis, como diz  tabela
-						inst[2][0] = '0';
-						istringstream(inst[2]) >> valor;
-						valor &= 0b0000000001111111;
+    if (in.is_open())
+    {
+        while (getline(in, line))
+        {
+            transform(line.begin(), line.end(), line.begin(), ::toupper); //tranforma tudo em maiúscula
+            vector<string> inst = split(line, " ");
+            out.open("out.txt");
+            if (inst[0] == "MOV")
+            {
+                if (inst[1][0] == 'R')
+                {
+                    if (inst[2][0] == '#')
+                    { //identificou a instrução MOV da 5º linha da tabela
+                        //colocando o valor imediato no short(16 bits) nos 7 primeiros bitis, como diz  tabela
+                        inst[2][0] = '0';
+                        istringstream(inst[2]) >> valor;
+                        valor &= 0b0000000001111111;
 
-						//colocando o numero do registrador nos bits(8..10)
-						int reg = 0;
-						reg = inst[1][1] - 48;
-						valor |= reg << 8;
-						valor &= 0b0000001111111111;
+                        //colocando o numero do registrador nos bits(8..10)
+                        int reg = 0;
+                        reg = inst[1][1] - 48;
+                        valor |= reg << 8;
+                        valor &= 0b0000001111111111;
 
-						//colocando o restante dos bits com o op(0) já que é a primeira instrução da linha
-						valor |= 0b00100 << 11;
-						printf("%x\n", valor);
-						out << valor << "\n";
-					}
-				}
-			}
-			else if (inst[0] == "CMP")
-			{
-				if (inst[1][0] == 'R')
-				{
-					if (inst[2][0] == '#')
-					{
-						inst[2][0] = '0';
-						istringstream(inst[2]) >> valor;
-						valor &= 0b0000000001111111;
+                        //colocando o restante dos bits com o op(0) já que é a primeira instrução da linha
+                        valor |= 0b00100 << 11;
+                        printf("%x\n", valor);
+                        out << valor << "\n";
+                    }
+                }
+            }
+            else if (inst[0] == "CMP")
+            {
+                if (inst[1][0] == 'R')
+                {
+                    if (inst[2][0] == '#')
+                    {
+                        inst[2][0] = '0';
+                        istringstream(inst[2]) >> valor;
+                        valor &= 0b0000000001111111;
 
-						int reg = 0;
-						reg = inst[1][1] - 48;
-						valor |= reg << 8;
-						valor &= 0b0000001111111111;
+                        int reg = 0;
+                        reg = inst[1][1] - 48;
+                        valor |= reg << 8;
+                        valor &= 0b0000001111111111;
 
-						valor |= 0b00101 << 11;
-						printf("%x\n", valor);
-						out << valor << "\n";
-					}
-				}
-			}
-			else if (inst[0] == "ADD")
-			{
-				if (inst[1][0] == 'R')
-				{
-					if (inst[2][0] == '#')
-					{
-						inst[2][0] = '0';
-						istringstream(inst[2]) >> valor;
-						valor &= 0b0000000001111111;
+                        valor |= 0b00101 << 11;
+                        printf("%x\n", valor);
+                        out << valor << "\n";
+                    }
+                }
+            }
+            else if (inst[0] == "ADD")
+            {
+                if (inst[1][0] == 'R')
+                {
+                    if (inst[2][0] == '#')
+                    {
+                        inst[2][0] = '0';
+                        istringstream(inst[2]) >> valor;
+                        valor &= 0b0000000001111111;
 
-						int reg = 0;
-						reg = inst[1][1] - 48;
-						valor |= reg << 8;
-						valor &= 0b0000001111111111;
+                        int reg = 0;
+                        reg = inst[1][1] - 48;
+                        valor |= reg << 8;
+                        valor &= 0b0000001111111111;
 
-						valor |= 0b00110 << 11;
-						printf("%x\n", valor);
-						out << valor << "\n";
-					}
-				}
-			}
-			else if (inst[0] == "SUB")
-			{
-				if (inst[1][0] == 'R')
-				{
-					if (inst[2][0] == '#')
-					{
-						inst[2][0] = '0';
-						istringstream(inst[2]) >> valor;
-						valor &= 0b0000000001111111;
+                        valor |= 0b00110 << 11;
+                        printf("%x\n", valor);
+                        out << valor << "\n";
+                    }
+                }
+            }
+            else if (inst[0] == "SUB")
+            {
+                if (inst[1][0] == 'R')
+                {
+                    if (inst[2][0] == '#')
+                    {
+                        inst[2][0] = '0';
+                        istringstream(inst[2]) >> valor;
+                        valor &= 0b0000000001111111;
 
-						int reg = 0;
-						reg = inst[1][1] - 48;
-						valor |= reg << 8;
-						valor &= 0b0000001111111111;
+                        int reg = 0;
+                        reg = inst[1][1] - 48;
+                        valor |= reg << 8;
+                        valor &= 0b0000001111111111;
 
-						valor |= 0b00111 << 11;
-						printf("%x\n", valor);
-						out << valor << "\n";
-					}
-				}
-			}
-		}
-	}
+                        valor |= 0b00111 << 11;
+                        printf("%x\n", valor);
+                        out << valor << "\n";
+                    }
+                }
+            }
+            else if (inst[0] == "LDR")
+            {
+                if (inst[1][0] == 'R')
+                {
+                    if (inst[2][0] == '#')
+                    {
+                        inst[2][0] = '0';
+                        istringstream(inst[2]) >> valor;
+                        valor &= 0b0000000001111111;
 
-	out.close();
-	in.close();
+                        int reg = 0;
+                        reg = inst[1][1] - 48;
+                        valor |= reg << 8;
+                        valor &= 0b0000001111111111;
 
-	return 0;
+                        valor |= 0b01001 << 11;
+                        printf("%x\n", valor);
+                        out << valor << "\n";
+                    }
+                }
+            }
+            else if (inst[0] == "ASR")
+            {
+                if (inst[1][0] == 'R')
+                {
+                    if (inst[2][0] == 'R')
+                    { //identificou a instrução ASR da 5º linha da tabela
+                        if (inst[3][0] == '#')
+                        {
+                            //colocando o valor imediato no short(16 bits) nos 7 primeiros bitis, como diz  tabela
+                            inst[2][0] = '0';
+                            istringstream(inst[2]) >> valor;
+                            valor &= 0b0000000000000011;
+
+                            inst[3][0] = '0';
+                            istringstream(inst[3]) >> valor;
+                            valor &= 0b0000000000010111;
+
+                            //colocando o numero do registrador nos bits(8..10)
+                            int reg = 0;
+                            reg = inst[1][1] - 48;
+                            valor |= reg << 8;
+                            valor &= 0b0000001111111111;
+
+                            //colocando o restante dos bits com o op(0) já que é a primeira instrução da linha
+                            valor |= 0b00010 << 11;
+                            printf("%x\n", valor);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    out.close();
+    in.close();
+
+    return 0;
 }
--- in.s
+++ in.s
@@ -1,9 +1,7 @@
 add r0, r1
 mov r2, #7
 cmp r0, #90
-feijjofe
-odjoocjo
-dojjcdoj
+asr r2, r0, #0xff
+ldr r0, #2000
 add r0, #1
-eifnkfn
 sub r1, #2
